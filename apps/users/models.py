2266240b17dc6a5af9983e512d962a911d72a067
Joel <huacreenciso97@gmail.com>
2023-02-19 21:51:54 -0500

    Create users app

---
+++ apps/users/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+
+
+class Profile(models.Model):
+    description = models.TextField(
+        null=True, blank=True, verbose_name=_("Descripcion"))
+    short_description = models.CharField(
+        max_length=50, null=True, blank=True, verbose_name=_("Descripcion corta"))
+    photo = models.ImageField(
+        upload_to="photos", null=True, blank=True, verbose_name=_("Foto"))
+    birth_date = models.DateField(
+        null=False, blank=False, verbose_name=_("Fecha de nacimiento"))
+    phone = models.CharField(
+        max_length=9, null=False, blank=False, verbose_name=_("Celular"))
+    email = models.EmailField(null=False, blank=False, verbose_name=_("Email"))
+    address = models.TextField(
+        null=False, blank=False, verbose_name=_("Dirección"))
+    country = models.CharField(
+        max_length=50, null=False, blank=False, verbose_name=_("País"))
+    linkedin = models.URLField(
+        null=False, blank=False, verbose_name=("Linkedin"))
+    github = models.URLField(null=False, blank=False, verbose_name=("Github"))
+
+    class Meta:
+        verbose_name = _("Perfil")
+        verbose_name_plural = _("Perfiles")
+
+
+class User(AbstractUser):
+    profile = models.OneToOneField(
+        Profile, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_("Perfil")
+    )
+
+    class Meta:
+        verbose_name = _("Usuario")
+        verbose_name_plural = _("Usuarios")
