4a76594863cf1099ac46c7aac1aaa1616ec8f1dc
Joel <2016014076@unfv.edu.pe>
2023-05-18 23:02:12 -0500

    Update requirements

--- resume/config/base.py
+++ resume/config/base.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
